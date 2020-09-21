eef9f6bac9fae7c9539abc5ec7bd97a4309ef92f
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-09-21 12:33:04 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,5 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
-git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat2
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcatV2
+git clone https://github.com/project-openwrt/luci-app-koolproxyR.git package/koolproxyR
