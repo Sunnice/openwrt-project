bd8200844369da6ceb931af6b04eea6cbbbceff5
Sunnice <44739306+Sunnice@users.noreply.github.com>
2020-09-21 12:08:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,4 @@
 
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+git clone https://github.com/Leo-Jo-My/luci-theme-opentomcat.git package/luci-theme-opentomcat2
